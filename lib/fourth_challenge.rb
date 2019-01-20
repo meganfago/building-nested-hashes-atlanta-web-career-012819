a4d5b18c82aa3e32ca6aa304c994674ea2f53990
Megan Fago <meganlaynefago@gmail.com>
2019-01-20 20:06:16 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -21,11 +21,13 @@ def fourth_challenge
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
          {:name => "Steven",
-        :age => "30",
-           
+           :age => "30",
+           :attitude => "confused"
          },
-        # {:name => "Nurse",
-        # :age => "44"}
+         {:name => "Nurse",
+           :age => "44",
+           
+         }
         
       ]
    }
