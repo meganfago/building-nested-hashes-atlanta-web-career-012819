119fe9dc1804a8dc3ad399c40f9cfc5713461a5f
Megan Fago <meganlaynefago@gmail.com>
2019-01-20 19:59:08 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -31,7 +31,8 @@ def third_challenge
         },
       :heroine => {
         :name => "Juliet",
-        :age => "15"
+        :age => "15",
+        :status => "alive"
         },
       :heroine_friends => [
         {:name => "Steven",
